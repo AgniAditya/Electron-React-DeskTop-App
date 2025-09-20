a130e860d1b545ea25674a845747a1ad012669b5
AgniAditya <agniaditya05@gmail.com>
2025-09-20 12:38:59 +0530

    feat: initialize Electron and React application with Vite
    - Added package.json with scripts for development, build, and distribution.
    - Created main Electron process in src/electron/main.ts.
    - Added TypeScript configuration for Electron in src/electron/tsconfig.json.
    - Implemented utility function to check development mode in src/electron/utils.ts.
    - Developed basic React application structure in src/renderer with App component and styles.
    - Included SVG logo asset for React in src/renderer/assets/react.svg.
    - Set up global styles in src/renderer/index.css.
    - Configured Vite for building and serving the application in vite.config.ts.
    - Established TypeScript configurations for application and Node in tsconfig.app.json and tsconfig.node.json.

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import {app , BrowserWindow} from 'electron';
+import path from 'path'
+import { isDev } from './utils.js'
+
+type test = string;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+
+    })
+    if(isDev()){
+        mainWindow.loadURL('http://localhost:8000');
+    }
+    else{
+        mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html'))
+    }
+})

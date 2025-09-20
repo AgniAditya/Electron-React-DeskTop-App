0b9d7e3989bece490132a10d0e302ca1d7d3a1a6
AgniAditya <agniaditya05@gmail.com>
2025-09-20 13:52:20 +0530

    feat: implement resource management functions and polling mechanism

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,14 @@
 import {app , BrowserWindow} from 'electron';
 import path from 'path'
 import { isDev } from './utils.js'
-
-type test = string;
+import { getAppPath } from './pathResolver.js';
+import { pollResources } from './resourceManager.js';
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({
-
+        webPreferences: {
+            preload: getAppPath()
+        }
     })
     if(isDev()){
         mainWindow.loadURL('http://localhost:8000');
@@ -14,4 +16,6 @@ app.on('ready', () => {
     else{
         mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html'))
     }
+
+    pollResources()
 })
